9ab88ee14348326f46764e7477e2d468dc54d83c
xakepok@xakepok.com <44duP_yx579>
2018-10-26 22:56:50 +0300

    Minimize code for save data in tables

--- administrator/components/com_projects/models/exhibitor.php
+++ administrator/components/com_projects/models/exhibitor.php
@@ -1,8 +1,10 @@
 <?php
 defined('_JEXEC') or die;
+
 use Joomla\CMS\MVC\Model\AdminModel;
 
-class ProjectsModelExhibitor extends AdminModel {
+class ProjectsModelExhibitor extends AdminModel
+{
     public function getTable($name = 'Exponents', $prefix = 'TableProjects', $options = array())
     {
         $this->addTablePath(JPATH_COMPONENT_ADMINISTRATOR . '/tables');
@@ -40,15 +42,14 @@ class ProjectsModelExhibitor extends AdminModel {
     public function getForm($data = array(), $loadData = true)
     {
         $form = $this->loadForm(
-            $this->option.'.exhibitor', 'exhibitor', array('control' => 'jform', 'load_data' => $loadData)
+            $this->option . '.exhibitor', 'exhibitor', array('control' => 'jform', 'load_data' => $loadData)
         );
-        if (empty($form))
-        {
+        if (empty($form)) {
             return false;
         }
         $id = JFactory::getApplication()->input->get('id', 0);
         $user = JFactory::getUser();
-        if ($id != 0 && (!$user->authorise('core.edit.state', $this->option . '.exhibitor.' . (int) $id))
+        if ($id != 0 && (!$user->authorise('core.edit.state', $this->option . '.exhibitor.' . (int)$id))
             || ($id == 0 && !$user->authorise('core.edit.state', $this->option)))
             $form->setFieldAttribute('state', 'disabled', 'true');
 
@@ -57,9 +58,8 @@ class ProjectsModelExhibitor extends AdminModel {
 
     protected function loadFormData()
     {
-        $data = JFactory::getApplication()->getUserState($this->option.'.edit.exhibitor.data', array());
-        if (empty($data))
-        {
+        $data = JFactory::getApplication()->getUserState($this->option . '.edit.exhibitor.data', array());
+        if (empty($data)) {
             $data = $this->getItem();
         }
 
@@ -68,11 +68,10 @@ class ProjectsModelExhibitor extends AdminModel {
 
     protected function prepareTable($table)
     {
-    	$nulls = array('title_ru_short', 'title_ru_full', 'title_en'); //Поля, которые NULL
-	    foreach ($nulls as $field)
-	    {
-		    if (!strlen($table->$field)) $table->$field = NULL;
-    	}
+        $nulls = array('title_ru_short', 'title_ru_full', 'title_en'); //Поля, которые NULL
+        foreach ($nulls as $field) {
+            if (!strlen($table->$field)) $table->$field = NULL;
+        }
         parent::prepareTable($table);
     }
 
@@ -80,12 +79,9 @@ class ProjectsModelExhibitor extends AdminModel {
     {
         $user = JFactory::getUser();
 
-        if (!empty($record->id))
-        {
-            return $user->authorise('core.edit.state', $this->option . '.exhibitor.' . (int) $record->id);
-        }
-        else
-        {
+        if (!empty($record->id)) {
+            return $user->authorise('core.edit.state', $this->option . '.exhibitor.' . (int)$record->id);
+        } else {
             return parent::canEditState($record);
         }
     }
@@ -97,54 +93,13 @@ class ProjectsModelExhibitor extends AdminModel {
 
     public function save($data)
     {
-        $fields = array(
-            'general' => array(
-                'id' => $data['id'],
-                'regID' => $data['regID'],
-                'curatorID' => $data['curatorID'],
-                'title_ru_full' => $data['title_ru_full'],
-                'title_ru_short' => $data['title_ru_short'],
-                'title_en' => $data['title_en'],
-                'state' => $data['state'],
-            ),
-            'bank' => array(
-                'inn' => $data['inn'],
-                'kpp' => $data['kpp'],
-                'rs' => $data['rs'],
-                'ks' => $data['ks'],
-                'bank' => $data['bank'],
-                'bik' => $data['bik'],
-            ),
-            'contact' => array(
-                'addr_legal_ru' => $data['addr_legal_ru'],
-                'addr_legal_en' => $data['addr_legal_en'],
-                'addr_fact' => $data['addr_fact'],
-                'phone_1' => $data['phone_1'],
-                'phone_2' => $data['phone_2'],
-                'fax' => $data['fax'],
-                'email' => $data['email'],
-                'site' => $data['site'],
-                'director_name' => $data['director_name'],
-                'director_post' => $data['director_post'],
-                'contact_person' => $data['contact_person'],
-                'contact_data' => $data['contact_data'],
-            )
-        );
-        $s1 = parent::save($fields['general']);
+        $fields = $this->filterFields($this->getTable(), $data);
+        $s1 = parent::save($fields);
 
-        $id = (JFactory::getApplication()->input->getInt('id', 0) == 0) ? $this->getTable()->getDbo()->insertid() : JFactory::getApplication()->input->getInt('id');
-        $adv1 = $this->getFieldName('Banks', $id);
-        $adv2 = $this->getFieldName('Addresses', $id);
-        $fields['bank'][$adv1['field']] = $adv1['value'];
-        $fields['contact'][$adv2['field']] = $adv2['value'];
-        if ($id !== 0)
-        {
-            $fields['bank']['exbID'] = $id;
-            $fields['contact']['exbID'] = $id;
-        }
+        $data = $this->addId($data);
 
-        $s2 = $this->saveData('Bank', $fields['bank']);
-        $s3 = $this->saveData('Address', $fields['contact']);
+        $s2 = $this->saveData('Bank', $data);
+        $s3 = $this->saveData('Address', $data);
         return $s1 && $s2 && $s3;
     }
 
@@ -153,65 +108,36 @@ class ProjectsModelExhibitor extends AdminModel {
     {
         $model = AdminModel::getInstance($modelName, 'ProjectsModel');
         $table = $model->getTable();
+        $data = $this->filterFields($table, $data);
         $table->bind($data);
         $model->prepareTable($table);
         return $model->save($data);
     }
 
-    /*Определяем вставляем запись в дочерней таблице или обновляем имеюющуюся*/
-    private function getFieldName(string $tblName, int $id): array
+    /*Отсиеваем ненужные поля перед привязкой к таблице*/
+    private function filterFields(object $table, array $data): array
     {
-        $result = array();
-        $table = $this->getTable($tblName, 'TableProjects');
-        $table->load(array('exbID' => $id));
-        if ($table->id == null)
-        {
-            $result['field'] = 'exbID';
-            $result['value'] = $id;
+        foreach ($data as $field => $value) {
+            if (!property_exists($table, $field)) unset($data[$field]);
         }
-        else
-        {
-            $result['field'] = 'id';
-            $result['value'] = $table->id;
-        }
-        return $result;
+        return $data;
     }
 
-    /*
-     * Сохраняем банковские реквизиты
-     * МЕТОД НЕ ИСПОЛЬЗУЕТСЯ. НУЖДАЕТСЯ В УДАЛЕНИИ.
-     * */
-    /*
-    private function saveBank($data, int $id = 0)
+    /*Добавляем поле с id записи, если нужно обновить её в дочерней таблице, а не добавить*/
+    private function addId(array $data): array
     {
-        $data['exbID'] = $id;
-        unset($data['regID'], $data['curatorID'], $data['title_ru_full'], $data['title_ru_short'], $data['title_en'], $data['state'], $data['tags'], $data['id']);
-        $db =& $this->getDbo();
-        $table = $db->quoteName("#__prj_exp_bank");
-        $arr = array(
-            $db->quoteName('exbID') => $db->quote($id),
-            $db->quoteName('inn') => (!empty($data['inn'])) ? $db->quote($data['inn']) : 'NULL',
-            $db->quoteName('kpp') => (!empty($data['kpp'])) ? $db->quote($data['kpp']) : 'NULL',
-            $db->quoteName('rs') => (!empty($data['rs'])) ? $db->quote($data['rs']) : 'NULL',
-            $db->quoteName('ks') => (!empty($data['ks'])) ? $db->quote($data['ks']) : 'NULL',
-            $db->quoteName('bank') => (!empty($data['bank'])) ? $db->quote($db->escape($data['bank'])) : 'NULL',
-            $db->quoteName('bik') => (!empty($data['bik'])) ? $db->quote($data['bik']) : 'NULL',
-        );
-        $columns = array();
-        $values = array();
-        $set = array();
-        foreach ($arr as $item => $value)
-        {
-            array_push($columns, $item);
-            array_push($values, $value);
-            array_push($set, sprintf("%s = %s", $item, $value));
-        }
-        $columns = implode(', ', $columns);
-        $values = implode(', ', $values);
-        $set = implode(', ', $set);
-        $query = "INSERT INTO {$table} ({$columns}) VALUES ({$values}) ";
-        unset($arr[$db->quoteName('exbID')]);
-        $query .= "ON DUPLICATE KEY UPDATE {$set}";
-        return $db->setQuery($query)->execute();
-    }*/
+        $id = $this->getId();
+        if ($id !== 0) $data['exbID'] = $id;
+        $table = $this->getTable('Banks', 'TableProjects');
+        $table->load(array('exbID' => $id));
+        if ($table->id != null) $data['id'] = $table->id;
+        return $data;
+    }
+
+    /*Получаем ИД записи в таблице*/
+    private function getId(): int
+    {
+        return (JFactory::getApplication()->input->getInt('id', 0) == 0) ? $this->getTable()->getDbo()->insertid() : JFactory::getApplication()->input->getInt('id', 0);
+    }
+
 }
