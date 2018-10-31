49c2f2da6a25e1b3ed46235cafca3a7ded364657
xakepok@xakepok.com <44duP_yx579>
2018-10-31 17:37:43 +0300

    In contract form avaliable only projects, where priceID is not null

--- administrator/components/com_projects/models/fields/project.php
+++ administrator/components/com_projects/models/fields/project.php
@@ -3,25 +3,30 @@ defined('_JEXEC') or die;
 jimport('joomla.form.helper');
 JFormHelper::loadFieldClass('list');
 
-class JFormFieldProject extends JFormFieldList  {
-    protected  $type = 'Project';
+class JFormFieldProject extends JFormFieldList
+{
+    protected $type = 'Project';
 
     protected function getOptions()
     {
-            $db =& JFactory::getDbo();
-            $query = $db->getQuery(true);
-            $query
-                ->select("`id`, `title`")
-                ->from('#__prj_projects')
-                ->order("`title`");
-            $result = $db->setQuery($query)->loadObjectList();
+        $view = JFactory::getApplication()->input->getString('view');
+        $db =& JFactory::getDbo();
+        $query = $db->getQuery(true);
+        $query
+            ->select("`id`, `title`")
+            ->from('#__prj_projects')
+            ->order("`title`");
+        if ($view == 'contract')
+        {
+            $query->where("`priceID` IS NOT NULL");
+        }
+        $result = $db->setQuery($query)->loadObjectList();
 
-            $options = array();
+        $options = array();
 
-            foreach ($result as $item)
-            {
-                $options[] = JHtml::_('select.option', $item->id, $item->title);
-            }
+        foreach ($result as $item) {
+            $options[] = JHtml::_('select.option', $item->id, $item->title);
+        }
 
         $options = array_merge(parent::getOptions(), $options);
 
