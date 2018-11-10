70f7299c7740dce288a40816f3a0ef2a2623e061
xakepok@xakepok.com <44duP_yx579>
2018-11-10 12:44:56 +0300

    Title in exponent's view

--- administrator/components/com_projects/views/exhibitor/view.html.php
+++ administrator/components/com_projects/views/exhibitor/view.html.php
@@ -6,6 +6,7 @@ class ProjectsViewExhibitor extends HtmlView {
     protected $item, $form, $script, $id, $activities;
 
     public function display($tmp = null) {
+        $this->id = JFactory::getApplication()->input->getInt('id', 0);
         $this->form = $this->get('Form');
         $this->item = $this->get('Item');
         $this->activities = $this->get('Activities');
@@ -19,7 +20,7 @@ class ProjectsViewExhibitor extends HtmlView {
 
     protected function addToolbar() {
         JFactory::getApplication()->input->set('hidemainmenu', true);
-        $title = $this->item->title ?? JText::sprintf('COM_PROJECTS_TITLE_NEW_EXHIBITOR');
+        $title = ($this->id != 0) ? ProjectsHelper::getExpTitle($this->item->title_ru_short, $this->item->title_ru_full, $this->item->title_en) : JText::sprintf('COM_PROJECTS_TITLE_NEW_EXHIBITOR');
 
         JToolbarHelper::title($title, '');
 	    JToolBarHelper::apply('exhibitor.apply', 'JTOOLBAR_APPLY');
@@ -28,7 +29,6 @@ class ProjectsViewExhibitor extends HtmlView {
     }
 
     protected function setDocument() {
-        //JHtml::_('jquery.framework');
         JHtml::_('bootstrap.framework');
     }
 }
