f0264a9ef5e634f81d6904c05f02085d5e87ae01
xakepok@xakepok.com <44duP_yx579>
2018-11-21 22:31:10 +0300

    Date only after save contract

--- administrator/components/com_projects/views/contract/tmpl/edit_general.php
+++ administrator/components/com_projects/views/contract/tmpl/edit_general.php
@@ -5,7 +5,7 @@ defined('_JEXEC') or die;
     <div class="control-group form-inline">
         <?php foreach ($this->form->getFieldset('names') as $field) :
             if ($field->name == 'jform[number]' && (!ProjectsHelper::canDo('projects.contract.allow') || $field->value == null)) continue;
-            if ($this->item->id == null && $field->name == 'jform[managerID]') continue;
+            if ($this->item->id == null && ($field->name == 'jform[managerID]' || $field->name == 'jform[dat]')) continue;
             ?>
             <div class="control-label"><?php echo $field->label; ?></div>
             <div class="controls">
