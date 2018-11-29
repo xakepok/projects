9c1740dca3ce6cd3168cf17a441b2e54b7325444
xakepok@xakepok.com <44duP_yx579>
2018-11-29 16:41:28 +0300

    Clicked email and site fields

--- administrator/components/com_projects/views/exhibitor/tmpl/edit_addresses.php
+++ administrator/components/com_projects/views/exhibitor/tmpl/edit_addresses.php
@@ -3,8 +3,19 @@ defined('_JEXEC') or die;
 ?>
 <fieldset class="adminform">
     <div class="control-group form-inline">
-        <?php foreach ($this->form->getFieldset('addresses') as $field) :
-            echo $field->renderField();
-        endforeach; ?>
+        <?php foreach ($this->form->getFieldset('addresses') as $field) : ?>
+            <div class="control-label">
+                <?php echo $field->label; ?>
+            </div>
+            <div class="controls">
+                <?php echo $field->input;
+                if (($field->name == 'jform[site]' || $field->name == 'jform[email]') && $field->value != null) {
+                    $url = ($field->name == 'jform[site]') ? $field->value : sprintf('mailto:%s', $field->value);
+                    echo JHtml::link($url, JText::sprintf('COM_PROJECTS_ACTION_GO'), array('target' => '_blank'));
+                }
+                ?>
+            </div>
+            <br>
+        <?php endforeach; ?>
     </div>
 </fieldset>
