5d18247c0f651eadb8fa77543747818753759c56
asharikov <asharikov@bmstu.ru>
2018-12-10 10:49:12 +0300

    MKV-43 Дефис в номере стенда

--- administrator/components/com_projects/models/forms/stand.js
+++ administrator/components/com_projects/models/forms/stand.js
@@ -1,1 +1,10 @@
 'use strict';
+window.onload = function () {
+    var field = document.querySelector("#jform_number");
+    field.addEventListener('keyup', upper, false);
+
+};
+function upper() {
+    var field = document.querySelector("#jform_number");
+    field.value = field.value.toUpperCase();
+}
