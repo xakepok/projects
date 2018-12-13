5697af061ab1b405b8e58453c9d5e5026910ca66
asharikov <asharikov@bmstu.ru>
2018-12-13 18:40:11 +0300

    MKV-53 Переименовать поле Дата в Дату договора

--- administrator/components/com_projects/views/contracts/tmpl/default_head.php
+++ administrator/components/com_projects/views/contracts/tmpl/default_head.php
@@ -29,7 +29,7 @@ $listDirn    = $this->escape($this->state->get('list.direction'));
         <?php echo JHtml::_('grid.sort', 'COM_PROJECTS_HEAD_CONTRACT_ACTIVE_TODOS', '`plan`', $listDirn, $listOrder); ?>
     </th>
     <th>
-        <?php echo JHtml::_('grid.sort', 'COM_PROJECTS_HEAD_CONTRACT_DATE', '`c`.`dat`', $listDirn, $listOrder); ?>
+        <?php echo JHtml::_('grid.sort', 'COM_PROJECTS_HEAD_CONTRACT_DATE_DOG', '`c`.`dat`', $listDirn, $listOrder); ?>
     </th>
     <th>
         <?php echo JHtml::_('grid.sort', 'COM_PROJECTS_HEAD_CONTRACT_MANAGER', '`manager`', $listDirn, $listOrder); ?>
