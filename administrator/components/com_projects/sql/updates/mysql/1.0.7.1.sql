f194ccde2c57b8d20bcb4692a1a86fea54b3840f
asharikov <xakepok@xakepok.com>
2019-01-15 13:38:49 +0300

    Keys

--- administrator/components/com_projects/sql/updates/mysql/1.0.7.1.sql
+++ administrator/components/com_projects/sql/updates/mysql/1.0.7.1.sql
@@ -6,3 +6,20 @@ create table `#__prj_catalog_titles`
     primary key (id)
 )
   comment 'Список каталогов стендов';
+INSERT INTO `#__prj_catalog_titles` (id, title) VALUES (1, 'Первый каталог');
+
+alter table `#__prj_catalog`
+  add titleID int default 1 not null after id;
+
+create index `#__prj_catalog_titleID_index`
+  on `#__prj_catalog` (titleID);
+
+drop index number on `#__prj_catalog`;
+
+create unique index number
+  on `#__prj_catalog` (number, titleID);
+
+alter table `#__prj_catalog`
+  add constraint `#__prj_catalog_#__prj_catalog_titles_id_fk`
+    foreign key (titleID) references `#__prj_catalog_titles` (id)
+      on update cascade on delete cascade;
