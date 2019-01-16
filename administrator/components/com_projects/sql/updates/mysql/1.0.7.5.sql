79d89d48d68a339c34ac0617435103058a73a7c9
asharikov <xakepok@xakepok.com>
2019-01-16 12:08:06 +0300

    price and ctritems values is decimical

---
+++ administrator/components/com_projects/sql/updates/mysql/1.0.7.5.sql
@@ -0,0 +1,7 @@
+alter table `#__prc_items` modify column_1 decimal(3,2) default 1.00 not null comment 'Коэффициент наценки в колонке 1';
+alter table `#__prc_items` modify column_2 decimal(3,2) default 1.5 not null comment 'Коэффициент наценки в колонке 2';
+alter table `#__prc_items` modify column_3 decimal(3,2) default 2.00 not null comment 'Коэффициент наценки в колонке 2';
+alter table `#__prj_contract_items` modify factor decimal(3,2) default 1 not null comment 'Множитель первой цены';
+alter table `#__prj_contract_items` modify markup decimal(3,2) null comment 'Наценка за позиционирование';
+alter table `#__prj_contract_items` modify value decimal(9,2) not null comment 'Значение пункта из прайс-листа';
+alter table `#__prj_contract_items` modify value2 decimal(3,2) null comment 'Значение из второй единицы измерения';
