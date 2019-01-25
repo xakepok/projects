4d31f2bc8aa5ca5a7520cd741049de702d594ad6
asharikov <xakepok@xakepok.com>
2019-01-25 13:33:04 +0300

    ICE-11 Отображать номера стендов в списке заказанных услуг

--- administrator/components/com_projects/views/contract/tmpl/edit_summary.php
+++ administrator/components/com_projects/views/contract/tmpl/edit_summary.php
@@ -10,6 +10,9 @@ $sum = 0;
             <th>
                 <?php echo JText::sprintf('COM_PROJECTS_HEAD_ITEM_TITLE'); ?>
             </th>
+            <th style="width: 10%">
+                <?php echo JText::sprintf('COM_PROJECTS_HEAD_CONTRACT_STAND_NUMBER'); ?>
+            </th>
             <th style="width: 3%">
                 <?php echo JText::sprintf('COM_PROJECTS_HEAD_ITEM_PRICE_ITEMS_COUNT'); ?>
             </th>
@@ -32,7 +35,7 @@ $sum = 0;
             $subsum = 0;
             ?>
             <tr>
-                <td colspan="6" class="center"
+                <td colspan="7" class="center"
                     style="font-weight: bold;"><?php echo ProjectsHelper::getApplication($application); ?></td>
             </tr>
             <?php foreach ($sec as $section => $arr) :
@@ -44,6 +47,9 @@ $sum = 0;
                     <tr id="summary_<?php echo $item['id']; ?>" class="app_<?php echo $application;?> <?php echo $dsp; ?>" data-app="<?php echo $application;?>">
                         <td>
                             <?php echo $item['title']; ?>
+                        </td>
+                        <td class="price_cost">
+                            <?php echo $item['stand']; ?>
                         </td>
                         <td>
                             <span id="sum_cnt_<?php echo $item['id']; ?>"><?php echo $item['value']; ?></span>&nbsp;<?php echo $item['unit']; ?>
@@ -67,7 +73,7 @@ $sum = 0;
                 endforeach;
             endforeach; ?>
             <tr>
-                <td colspan="5"
+                <td colspan="6"
                     style="text-align: right; font-weight: bold;"><?php echo JText::sprintf('COM_PROJECTS_HEAD_CONTRACT_SUBSUM'); ?></td>
                 <td>
                     <span id="subsumapp_<?php echo $application;?>" style="display: none;"><?php echo $subsum; ?></span>
@@ -79,7 +85,7 @@ $sum = 0;
         </tbody>
         <tfoor>
             <tr>
-                <td colspan="6" style="text-align: right; font-weight: bold;">
+                <td colspan="7" style="text-align: right; font-weight: bold;">
                     <?php echo JText::sprintf('COM_PROJECTS_HEAD_CONTRACT_SUM'); ?>:&nbsp;
                     <span id="sum_amountS" style="display: none;"><?php echo $sum;?></span>
                     <span id="sum_amountSV"><?php echo number_format($sum, 2, ',', ' ');?></span>
