5aa2a64ed1c65a9373604046264deaccd680a4b3
asharikov <xakepok@xakepok.com>
2019-01-28 15:43:43 +0300

    ICE-17 Сохранение активной вкладки после сохранения

--- administrator/components/com_projects/models/forms/exhibitor.js
+++ administrator/components/com_projects/models/forms/exhibitor.js
@@ -10,6 +10,19 @@ window.onload = function () {
     inn.addEventListener('keyup', checkExp, false);
     setMask('mask_jform[phone_1]', 'jform_phone_1');
     setMask('mask_jform[phone_2]', 'jform_phone_2');
+
+    // for bootstrap 3 use 'shown.bs.tab', for bootstrap 2 use 'shown' in the next line
+    jQuery('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+        // save the latest tab; use cookies if you like 'em better:
+        localStorage.setItem('lastTab', jQuery(this).attr('href'));
+        console.log(jQuery(this).attr('href'));
+    });
+
+    // go to the latest tab, if it exists:
+    var lastTab = localStorage.getItem('lastTab');
+    if (lastTab) {
+        jQuery('[href="' + lastTab + '"]').tab('show');
+    }
 };
 
 function copyAddr() {
