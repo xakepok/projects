e92b7c73d453b395c98f3bd8fc25b8cd1796d4cf
asharikov <xakepok@xakepok.com>
2019-01-28 14:07:36 +0300

    ICE-16 Неправильно кодируется URL для возврата

--- administrator/components/com_projects/views/exhibitor/tmpl/edit_contacts.php
+++ administrator/components/com_projects/views/exhibitor/tmpl/edit_contacts.php
@@ -1,6 +1,6 @@
 <?php
 defined('_JEXEC') or die;
-$return = base64_encode(JUri::base() . "index.php?option=com_projects&view=exhibitor&layout=edit&id={$this->item->id}");
+$return = base64_encode("index.php?option=com_projects&view=exhibitor&layout=edit&id={$this->item->id}");
 if ($this->item->id != null) {
     $addUrl = JRoute::_("index.php?option=com_projects&amp;task=person.add&amp;exbID={$this->item->id}&amp;return={$return}");
     $addLink = JHtml::link($addUrl, JText::sprintf('COM_PROJECTS_TITLE_NEW_PERSON'));
