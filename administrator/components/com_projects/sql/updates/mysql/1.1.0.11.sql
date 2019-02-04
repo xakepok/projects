c3ce34314b5d1595ff56d1c052719f4afc9ed1f8
asharikov <xakepok@xakepok.com>
2019-02-04 15:00:19 +0300

    Hotels and numbers tables

--- administrator/components/com_projects/sql/updates/mysql/1.1.0.11.sql
+++ administrator/components/com_projects/sql/updates/mysql/1.1.0.11.sql
@@ -1,2 +1,67 @@
-alter table `mfmza_prj_catalog_titles`
+alter table `#__prj_catalog_titles`
   add tip tinyint default 0 not null comment ' 0 - стенд, 1 - номер ' after id;
+
+create table `#__prj_hotels`
+(
+  id int auto_increment,
+  title text null,
+  constraint `#__prj_hotels_pk`
+    primary key (id)
+)
+  comment 'Отели';
+
+insert into `#__prj_hotels`
+(`id`, `title`)
+VALUES
+(1, 'Комплекс отдыха Бекасово-spa'),
+(2, 'Les Art Resolt'),
+(3, 'Дом отдыха Покровское'),
+(4, 'Heliopark'),
+(5, 'Парк-отель Ершово');
+
+create table `#__prj_hotels_number_categories`
+(
+  id int auto_increment,
+  hotelID int not null comment 'ID отеля',
+  title_ru text not null comment 'Название категории',
+  title_en text default null null comment 'Category title',
+  constraint `#__prj_hotels_number_categories_pk`
+    primary key (id),
+  constraint `#__prj_hotels_number_categories_#__prj_hotels_id_fk`
+    foreign key (hotelID) references `#__prj_hotels` (id)
+      on update cascade on delete cascade
+)
+  comment 'Категории номеров в отелях';
+
+create index `#__prj_hotels_number_categories_hotelID_index`
+  on `#__prj_hotels_number_categories` (hotelID);
+
+insert into `#__prj_hotels_number_categories`
+(`id`, `hotelID`, `title_ru`)
+values
+(NULL, '1', 'Стандарт однокомнатный'),
+(NULL, '1', 'Студия'),
+(NULL, '1', 'Двухкомнатный номер'),
+(NULL, '1', 'Трёхкомнатный номер апартамент'),
+(NULL, '1', 'Стандарт мансардный однокомнатный'),
+(NULL, '1', 'Двухкомнатный номер люкс'),
+(NULL, '1', 'Трёхкомнатный номер дуплекс'),
+(NULL, '1', 'Коттедж гостинный дом - двухкомнатный номер 37 кв. м.'),
+(NULL, '1', 'Коттедж гостинный дом - двухкомнатный номер люкс 53 кв. м.'),
+(NULL, '1', 'Отдельный коттедж'),
+(NULL, '2', 'Стандарт'),
+(NULL, '2', 'Симпл сьют'),
+(NULL, '2', 'Панорама сьют'),
+(NULL, '2', 'Классик сьют+'),
+(NULL, '2', 'Villa'),
+(NULL, '3', 'Корпус №2 (VIP)'),
+(NULL, '3', 'Корпус №1 (после ремонта)'),
+(NULL, '3', 'Корпус №3'),
+(NULL, '3', 'Таун-хаус №1'),
+(NULL, '3', 'Villa'),
+(NULL, '4', 'Стандарт 11 кв. м.'),
+(NULL, '4', 'Стандарт 11-14 кв. м.'),
+(NULL, '4', 'Стандарт расширенный'),
+(NULL, '4', 'Полулюкс'),
+(NULL, '5', 'Полулюкс'),
+(NULL, '5', 'Люкс двухкомнатный');
