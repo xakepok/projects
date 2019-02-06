306d77dd3c55b5f9212d65341d20206c0792a1f7
asharikov <xakepok@xakepok.com>
2019-02-06 14:49:28 +0300

    Version 1.1.1.9

--- administrator/components/com_projects/views/event/tmpl/edit_compare.php
+++ administrator/components/com_projects/views/event/tmpl/edit_compare.php
@@ -2,25 +2,32 @@
 defined('_JEXEC') or die;
 $arr_old = json_decode($this->item->old_data, true);
 $arr_new = $this->item->params;
-$diff_1 = array_diff($arr_old, $arr_new);
-$diff_2 = array_diff($arr_new, $arr_old);
-$keys = array_keys($arr_old) + array_keys($arr_new);
+if ($this->item->action != 'edit') {
+    $diff_1 = array_diff($arr_old, $arr_new);
+    $diff_2 = array_diff($arr_new, $arr_old);
+}
+$keys = ($this->item->action != 'add') ? array_keys($arr_old) + array_keys($arr_new) : array_keys($arr_new);
 ?>
 <table class="table table-striped">
     <thead>
     <tr>
-        <th><?php echo JText::sprintf('COM_PROJECTS_HEAD_PARAM');?></th>
-        <th><?php echo JText::sprintf('COM_PROJECTS_HEAD_WAS');?></th>
-        <th><?php echo JText::sprintf('COM_PROJECTS_HEAD_IS');?></th>
+        <th><?php echo JText::sprintf('COM_PROJECTS_HEAD_PARAM'); ?></th>
+        <th><?php echo JText::sprintf('COM_PROJECTS_HEAD_WAS'); ?></th>
+        <th><?php echo JText::sprintf('COM_PROJECTS_HEAD_IS'); ?></th>
     </tr>
     </thead>
     <tbody>
     <?php foreach ($keys as $key) : ?>
-    <tr style="color: <?php echo ($arr_old[$key] == $arr_new[$key]) ? 'black' : 'red';?>">
-        <td><?php echo $key;?></td>
-        <td><?php echo $arr_old[$key];?></td>
-        <td><?php echo $arr_new[$key];?></td>
-    </tr>
+        <?php if ($this->item->action != 'edit'): ?>
+            <tr>
+        <?php endif; ?>
+        <?php if ($this->item->action == 'edit'): ?>
+            <tr style="color: <?php echo ($arr_old[$key] == $arr_new[$key]) ? 'black' : 'red'; ?>">
+        <?php endif; ?>
+        <td><?php echo $key; ?></td>
+        <td><?php echo $arr_old[$key]; ?></td>
+        <td><?php echo $arr_new[$key]; ?></td>
+        </tr>
     <?php endforeach; ?>
     </tbody>
 </table>
