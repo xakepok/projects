9e9d458b6425e751ea12ff8b42df13a95a1a308e
asharikov <xakepok@xakepok.com>
2019-02-22 12:52:41 +0300

    Version 1.1.3.3

--- administrator/components/com_projects/sql/updates/mysql/1.1.3.3.sql
+++ administrator/components/com_projects/sql/updates/mysql/1.1.3.3.sql
@@ -1,10 +1,10 @@
 create view `#__prj_tmp_acts` as
 SELECT `c`.`id` as `contractID`, `c`.`expID`, `act`.`title`, `r`.`id` as `rubricID`
 FROM `#__prj_contracts` as `c`
-       LEFT JOIN `#__prj_exp_act` as `a` ON `a`.`id` = `c`.`expID`
+       RIGHT JOIN `#__prj_exp_act` as `a` ON `a`.`exbID` = `c`.`expID`
        LEFT JOIN `#__prj_activities` as `act` ON `act`.`id` = `a`.`actID`
-       LEFT JOIN `#__prj_rubrics` as `r` ON `r`.`title` = `act`.`title`
-WHERE `r`.`id` IS NOT NULL;
+       LEFT JOIN `#__prj_rubrics` as `r` ON `r`.`title` collate utf8_general_ci = `act`.`title` collate utf8_general_ci
+WHERE `r`.`id` IS NOT NULL AND `a`.`id` is not null;
 
 INSERT INTO `#__prj_contract_rubrics` (`contractID`, `rubricID`) SELECT `contractID`, `rubricID` FROM `#__prj_tmp_acts`;
 
