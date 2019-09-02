1f708d6eea61ac53763bcd5d6456d8a37b8cbe08
asharikov <xakepok@xakepok.com>
2019-09-02 17:48:26 +0300

    Forms catalog start

--- components/com_projects/views/forms/tmpl/default_catalog_site.php
+++ components/com_projects/views/forms/tmpl/default_catalog_site.php
@@ -2,4 +2,32 @@
 defined('_JEXEC') or die;
 ?>
 
-This forms for site catalog
+<div><h5><?php echo JText::sprintf('COM_PROJECTS_FORM_SITE_TITLE');?></h5></div>
+<div>
+    <form action="#">
+        <div class="form-group row">
+            <label for="title" class="col-md-2 col-form-label"><?php echo JText::sprintf('COM_PROJECTS_FORM_SITE_ORG_NAME');?></label>
+            <div class="col-md-10">
+                <input type="text" name="title" id="title" class="form-control">
+            </div>
+        </div>
+        <div class="form-group row">
+            <label for="diploma" class="col-md-2 col-form-label"><?php echo JText::sprintf('COM_PROJECTS_FORM_SITE_ORG_NAME_DIPLOMA');?></label>
+            <div class="col-md-10">
+                <input type="text" name="diploma" id="diploma" class="form-control">
+            </div>
+        </div>
+        <div class="form-group row">
+            <label for="title_ru" class="col-md-2 col-form-label"><?php echo JText::sprintf('COM_PROJECTS_FORM_SITE_ORG_NAME_RU');?></label>
+            <div class="col-md-10">
+                <input type="text" name="title_ru" id="title_ru" class="form-control">
+            </div>
+        </div>
+        <div class="form-group row">
+            <label for="title_en" class="col-md-2 col-form-label"><?php echo JText::sprintf('COM_PROJECTS_FORM_SITE_ORG_NAME_EN');?></label>
+            <div class="col-md-10">
+                <input type="text" name="title_en" id="title_en" class="form-control">
+            </div>
+        </div>
+    </form>
+</div>
