9b27dca58eff5b08185b375da49d140c83757e54
asharikov <xakepok@xakepok.com>
2019-09-10 16:49:14 +0300

    Prefix

--- administrator/components/com_projects/sql/updates/mysql/1.2.6.0.sql
+++ administrator/components/com_projects/sql/updates/mysql/1.2.6.0.sql
@@ -1,16 +1,16 @@
-create or replace view `s7vi9_prj_contract_todos_count` as
+create or replace view `#__prj_contract_todos_count` as
 select contractID, ifnull(count(id),0) as cnt
-from `s7vi9_prj_todo_list`
+from `#__prj_todo_list`
 where (is_notify = 0 and state = 0)
 group by contractID;
 
-create or replace view `s7vi9_prj_contracts_min_dates` as
+create or replace view `#__prj_contracts_min_dates` as
 select contractID, min(dat) as dat
-from `s7vi9_prj_todos`
+from `#__prj_todos`
 where state = 0 and is_notify = 0
 group by contractID;
 
-create or replace view `s7vi9_prj_contracts_v2` as
+create or replace view `#__prj_contracts_v2` as
 select c.id, if(p.contract_prefix is null, ifnull(c.number,c.number_free), concat(p.contract_prefix, ifnull(c.number,c.number_free))) as num,
        c.dat, c.currency,
        c.isCoExp, c.parentID,
@@ -28,31 +28,31 @@ select c.id, if(p.contract_prefix is null, ifnull(c.number,c.number_free), conca
        ifnull(a.price,0)-ifnull(pay.payments,0) as debt,
        c.payerID, ifnull(e1.title_ru_short,ifnull(e1.title_ru_full,e1.title_en)) as payer,
        cmd.dat as plan_dat
-from `s7vi9_prj_contracts` as `c`
-         left join `s7vi9_prj_projects` `p` on c.prjID = `p`.id
-         left join `s7vi9_prj_exp` `e` on c.expID = `e`.id
-         left join `s7vi9_prj_exp` `coexp` on coexp.id = `c`.parentID
-         left join `s7vi9_prj_contract_todos_count` `tdc` on `tdc`.contractID = `c`.`id`
-         left join `s7vi9_users` `u` on c.managerID = `u`.id
-         left join `s7vi9_prj_statuses` `s` on `s`.`code` = `c`.`status`
-         left join `s7vi9_prj_contract_amounts` `a` on c.id = `a`.contractID
-         left join `s7vi9_prj_contract_payments` `pay` on c.id = `pay`.contractID
-         left join `s7vi9_prj_exp` `e1` on `e1`.id = `c`.`payerID`
-         left join `s7vi9_prj_contracts_min_dates` `cmd` on c.id = `cmd`.contractID;
+from `#__prj_contracts` as `c`
+         left join `#__prj_projects` `p` on c.prjID = `p`.id
+         left join `#__prj_exp` `e` on c.expID = `e`.id
+         left join `#__prj_exp` `coexp` on coexp.id = `c`.parentID
+         left join `#__prj_contract_todos_count` `tdc` on `tdc`.contractID = `c`.`id`
+         left join `#__users` `u` on c.managerID = `u`.id
+         left join `#__prj_statuses` `s` on `s`.`code` = `c`.`status`
+         left join `#__prj_contract_amounts` `a` on c.id = `a`.contractID
+         left join `#__prj_contract_payments` `pay` on c.id = `pay`.contractID
+         left join `#__prj_exp` `e1` on `e1`.id = `c`.`payerID`
+         left join `#__prj_contracts_min_dates` `cmd` on c.id = `cmd`.contractID;
 
-create table `s7vi9_prj_user_settings`
+create table `#__prj_user_settings`
 (
     id int auto_increment,
     userID int not null,
     params text default null null comment 'JSON настроек',
-    constraint `s7vi9_prj_user_settings_pk`
+    constraint `#__prj_user_settings_pk`
         primary key (id),
-    constraint `s7vi9_prj_user_settings_s7vi9_users_id_fk`
-        foreign key (userID) references `s7vi9_users` (id)
+    constraint `#__prj_user_settings_#__users_id_fk`
+        foreign key (userID) references `#__users` (id)
             on update cascade on delete cascade
 )
     comment 'Настройки пользователей';
 
-create unique index `s7vi9_prj_user_settings_userID_uindex`
-    on `s7vi9_prj_user_settings` (userID);
+create unique index `#__prj_user_settings_userID_uindex`
+    on `#__prj_user_settings` (userID);
 
