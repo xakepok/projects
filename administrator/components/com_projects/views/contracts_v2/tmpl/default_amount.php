a9ceeb8718da9e54f61051eb1b815ab70bb65d5b
asharikov <xakepok@xakepok.com>
2019-09-13 15:00:55 +0300

    Fix cols colspan

--- administrator/components/com_projects/views/contracts_v2/tmpl/default_amount.php
+++ administrator/components/com_projects/views/contracts_v2/tmpl/default_amount.php
@@ -2,7 +2,9 @@
 // Запрет прямого доступа.
 defined('_JEXEC') or die;
 $title = JText::sprintf('COM_PROJECTS_HEAD_CONTRACT_SUM_TOTAL');
-$colspan = $this->columnsCount - 3;
+$diff = (!$this->userSettings['contracts_v2-column_id']) ? 3 : 4;
+$last = (!$this->userSettings['contracts_v2-column_id']) ? 1 : 2;
+$colspan = $this->columnsCount - $diff;
 ?>
 <tr>
     <td colspan="<?php echo $colspan; ?>" style="text-align: right;">
@@ -14,7 +16,7 @@ $colspan = $this->columnsCount - 3;
     <td>
         <?php echo $this->items['total']['payments']['rub'];?>
     </td>
-    <td>
+    <td colspan="<?php echo $last;?>">
         <?php echo $this->items['total']['debt']['rub'];?>
     </td>
 </tr>
@@ -28,12 +30,12 @@ $colspan = $this->columnsCount - 3;
     <td>
         <?php echo $this->items['total']['payments']['usd'];?>
     </td>
-    <td>
+    <td colspan="<?php echo $last;?>">
         <?php echo $this->items['total']['debt']['usd'];?>
     </td>
 </tr>
 <tr>
-    <td colspan="<?php echo $colspan; ?>" style="text-align: right;">
+    <td colspan="<?php echo $colspan ?>" style="text-align: right;">
         <?php echo JText::sprintf('COM_PROJECTS_HEAD_ITEM_PRICE_EUR');?>
     </td>
     <td>
@@ -42,7 +44,7 @@ $colspan = $this->columnsCount - 3;
     <td>
         <?php echo $this->items['total']['payments']['eur'];?>
     </td>
-    <td>
+    <td colspan="<?php echo $last;?>">
         <?php echo $this->items['total']['debt']['eur'];?>
     </td>
 </tr>
